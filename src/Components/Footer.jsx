dbea67a3bf820c8a0bc8fc023dc4c25ba86ea5c0
SOMESHTHAPLIYALL <149586000+Qriocity-Company@users.noreply.github.com>
2024-07-31 14:36:26 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -23,17 +23,17 @@ const Footer = ({ onClick }) => {
   return (
     <footer className="fixed bottom-0 left-0 w-full shadow-md py-4 bg-[#cbf7ec]">
       <div className="container mx-auto px-4 flex justify-between items-center">
-        <div>
+        <div className="flex flex-col">
           <div className="flex items-center gap-2">
             <p className="text-[20px] md:text-[30px] font-bold">Free</p>
             <p className="text-[16px] md:text-[24px] font-bold line-through">
               $50
             </p>
-            <p className="text-[14px] md:text-[16px] text-gray-600">
-              Offer ends in{" "}
-              <span className="font-bold">{formatTime(timeLeft)}</span>
-            </p>
           </div>
+          <p className="text-[14px] md:text-[16px] text-gray-600">
+            Offer ends in{" "}
+            <span className="font-bold">{formatTime(timeLeft)}</span>
+          </p>
           <p className="text-[14px] md:text-[16px] text-gray-600">
             Deadline <span className="font-bold">July 30, 2024</span>
           </p>
